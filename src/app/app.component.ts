5b1002a3b63e7fd3f7ab7c449c9ca7b949277f59
Boris Thuy <boristhuy@gmail.com>
2021-02-05 11:08:45 +0100

    Encrypt/Decrypt using JSEncrypt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { JSEncrypt } from 'jsencrypt';
+import { privateKey, publicKey } from './keys';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-encrypt-decrypt';
+export class AppComponent implements OnInit {
+  input = '';
+  result = '';
+
+  jsEncrypt: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.jsEncrypt = new JSEncrypt();
+  }
+
+  encrypt(): void {
+    const text = `${this.input}`.trim();
+
+    this.jsEncrypt.setPublicKey(publicKey);
+    this.result = this.jsEncrypt.encrypt(text);
+  }
+
+  decrypt(): void {
+    this.jsEncrypt.setPrivateKey(privateKey);
+    this.result = this.jsEncrypt.decrypt(this.input);
+  }
 }
